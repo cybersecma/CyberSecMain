844731ad0546d5e39da34ba92a436fff35aac750
Youssef Akhiat <youssef.akhiat04@gmail.com>
2025-06-20 13:17:02 +0100

    added upcoming stream to be labled as past after the stream

--- src/data/streams.ts
+++ src/data/streams.ts
@@ -34,6 +34,17 @@ export const streams: Stream[] = [
     tags: ['trends', 'threats', 'analysis', 'cybersecurity', '2024', 'security-trends'],
     type: 'past'
   },
+  {
+    id: '3',
+    title: '5th CyberSec Hangout: Cyber governance & Security Consultant',
+    description: 'Join CyberSec Morocco 4th Hangout with ssi Mouhcine STITI a Senior Cyber Security Consultant, with 17 years of experience in the field- He is also CISSP, CISA, ISO 27001 LI / LA, MS Cybersecurity & Azure Architect Expert',
+    videoId: 'L9dZYoLYNfA',
+    date: '2025-06-21T16:00:00Z',
+    duration: '1.5 hours',
+    host: 'Mouhcine STITI',
+    tags: ['trends', 'threats', 'Security Consultant', 'cybersecurity', 'Cyber governance', 'security-trends'],
+    type: 'upcoming'
+  },
   
   
 ]; 
